3805478d819cde31ddb92630f1a172b5edd76664
NickySlicksHaha <nickengmann_2@yahoo.com>
2016-02-08 14:32:42 -0500

    Website Content
    Original Commit

---
+++ js/scrip.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function() {
+    $(window).stellar();
+    
+});
+
+$(document).ready(
+
+  function() { 
+
+    $("html").niceScroll({
+        cursorcolor:"rgba(30,30,30,.5)",
+        zindex:999,
+        scrollspeed:100,
+        mousescrollstep:50,
+        cursorborder:"0px solid #fff",
+    });
+      
+
+  }
+
+);
+
+
+
+
